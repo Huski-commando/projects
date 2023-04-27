c1d9ada2bc9586e999d9ff54875ac4ef84745aff
huski-commando <gyan.mu@gmail.com>
2023-04-27 09:45:06 +0530

    working on Tours project

---
+++ tours-project/src/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const url = 'https://course-api.com/react-tours-project';
+
+function App() {
+  useState();
+  return (
+    <div className="App">
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
